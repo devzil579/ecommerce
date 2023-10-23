487f1116d0e62ae791ed4112b1e4ff9979a25090
Nasir Zilan <devzil589@gmail.com>
2023-10-23 17:22:43 +0600

    4th commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,22 +5,35 @@ import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar =() =>{
     return (
-        <div className='navbar container'>
-            <div className='nav-logo'>
-                <img src={logo} alt=''/>
-                <p>SHOPPER</p>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary py-4">
+        <div className="container d-flex">
+            <div className='logo d-flex justify-content-start'>
+                <img className='' src={logo}/>
+                <p className=' fs-2'>SHOPPER</p>
             </div>
-            <ul className='nav-menu'>
-                <li>Shop</li>
-                <li>Men</li>
-                <li>Women</li>
-                <li>Kids</li>
+          <div className='navMenu d-flex justify-content-center'>
+            <ul className="navbar-nav d-flex justify-content-between me-auto mb-2 mb-lg-0">
+              <li className="nav-item px-5">
+                <a className="nav-link active" aria-current="page" href="#">Shop</a>
+              </li>
+              <li className="nav-item px-5">
+                <a className="nav-link" href="#">Men</a>
+              </li>
+              <li className="nav-item px-5">
+                <a className="nav-link" href="#">Women</a>
+              </li>
+              <li className="nav-item px-5">
+                <a className="nav-link" href="#">Kids</a>
+              </li>
             </ul>
-            <div className='nav-login-cart'>
-                <button>Login</button>
-                <img src={cart_icon} alt=''></img>
-            </div>
+          </div>
+          <div className='loginAndCart d-flex justify-content-end'>
+          <button type="button" className="border px-4 btn btn-light px-4 py-2">Login</button>
+          <img src={cart_icon} className='px-4'/>
+          </div>
         </div>
+      </nav>
+   
     )
 }
 
