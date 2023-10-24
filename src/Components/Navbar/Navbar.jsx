2ca604f31d79f540569b37af2e3e4ca0e256f251
Nasir Zilan <devzil589@gmail.com>
2023-10-24 18:38:40 +0600

    6th commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from "react-router-dom";
 
 const Navbar =() =>{
+  const [menu, setMenu] = useState("shop")
+
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary py-4">
         <div className="container d-flex">
@@ -13,23 +16,15 @@ const Navbar =() =>{
             </div>
           <div className='navMenu d-flex justify-content-center'>
             <ul className="navbar-nav d-flex justify-content-between me-auto mb-2 mb-lg-0">
-              <li className="nav-item px-5">
-                <a className="nav-link active" aria-current="page" href="#">Shop</a>
-              </li>
-              <li className="nav-item px-5">
-                <a className="nav-link" href="#">Men</a>
-              </li>
-              <li className="nav-item px-5">
-                <a className="nav-link" href="#">Women</a>
-              </li>
-              <li className="nav-item px-5">
-                <a className="nav-link" href="#">Kids</a>
-              </li>
+            <li onCLick={()=>{setMenu("shop")}} className="nav-item px-5"> <Link class="text-decoration-none text-secondary-emphasis" to='/'>Shop </Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onCLick={()=>{setMenu("mens")}} className="nav-item px-5"><Link class="text-decoration-none text-secondary-emphasis" to='/mens'>Men  </Link>{menu==="mens"?<hr/>:<></>}</li>
+            <li onCLick={()=>{setMenu("womens")}} className="nav-item px-5">  <Link class="text-decoration-none text-secondary-emphasis" to='/womens'>Women </Link>{menu==="womens"?<hr/>:<></>}</li>
+            <li onCLick={()=>{setMenu("kids")}} className="nav-item px-5"><Link class="text-decoration-none text-secondary-emphasis" to='/kids'>Kids</Link> {menu==="kids"?<hr/>:<></>}</li>
             </ul>
           </div>
           <div className='loginAndCart d-flex justify-content-end'>
-          <button type="button" className="border px-4 btn btn-light px-4 py-2">Login</button>
-          <img src={cart_icon} className='px-4'/>
+          <button type="button" className="border px-4 btn btn-light px-4 py-2"><Link class="text-decoration-none text-secondary-emphasis"  to="/login">Login</Link></button>
+          <Link className="text-decoration-none" to='/cart'><img src={cart_icon} className='px-4'/></Link>
           </div>
         </div>
       </nav>
